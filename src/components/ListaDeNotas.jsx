9969f7f84ebb74db87959a096de03a1ad8914e82
Marllon Diniz <marllonzinid@gmail.com>
2022-03-15 20:42:58 -0300

    Aula2

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -4,17 +4,16 @@ import CardNota from "./CardNota";
 class ListaDeNotas extends Component {
   render() {
     return (
-      <ul>
-        <li>
-          <CardNota/>
-        </li>
-        <li>
-        <CardNota/>
-        </li>
-        <li>
-        <CardNota/>
-        </li>
-      </ul>
+    <ul>
+      {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+        return (
+          <li>
+            <div>{categoria}</div>
+            <CardNota />
+          </li>
+        );
+      })}
+    </ul>
     );
   }
 }
