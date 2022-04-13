ac4e51a53918687c292a98aa5b84fe3d838d13ba
Marllon <marllonzinid@gmail.com>
2022-04-13 13:32:09 -0300

    update CardNota.jsx, estilo.css and FormularioCadastro.jsx [master] Style delete

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -12,7 +12,9 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <DeleteSVG onClick={this.apagar.bind(this)} />
+          <button className="button-delete">
+            <DeleteSVG onClick={this.apagar.bind(this)} />
+          </button>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
